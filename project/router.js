a8d2538d1fdda748a0375b03c7fa677addf6b41b
hzq <saoraozhe3hao@126.com>
2016-03-22 21:46:59 +0800

    更新

--- project/router.js
+++ project/router.js
@@ -1,6 +1,6 @@
 //模块定义，第一个参数为模块ID，第二个参数为依赖数组，第三个参数为工厂函数
 //给工厂函数注入依赖。工厂函数中return 的就是这个模块定义要输出的对象
-define("project/router", ["project/top/topCtrl", "project/module/homeCtrl", "project/module/listCtrl", "project/module/detailCtrl"], function (topCtrl, homeCtrl, listCtrl, detailCtrl) {
+define("project/router", ["project/top/topCtrl", "project/module/homeCtrl", "project/module/detailCtrl"], function (topCtrl, homeCtrl, detailCtrl) {
         var app = angular.module("siwuyi", ['ui.router']).
             config(['$stateProvider', '$urlRouterProvider','$controllerProvider',
                 function ($stateProvider, $urlRouterProvider,$controllerProvider) {
@@ -18,7 +18,18 @@ define("project/router", ["project/top/topCtrl", "project/module/homeCtrl", "pro
                         .state('menu.list', {
                             url: '/list?topMenu&secondMenu',
                             templateUrl: 'views/list.html',
-                            controller: listCtrl
+                            controller: "listCtrl",
+                            //控制器懒加载
+                            resolve: {
+                                loadCtrl: ["$q", function($q) {
+                                    var deferred = $q.defer();
+                                    require(["project/module/listCtrl"], function(ctrl) {
+                                        $controllerProvider.register('listCtrl', ctrl);
+                                        deferred.resolve();
+                                    });
+                                    return deferred.promise;
+                                }]
+                            }
                         })
                         .state('menu.detail', {
                             url: '/detail?topMenu&secondMenu',
